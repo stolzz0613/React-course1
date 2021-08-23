d987a58e92a1e3db7e0780c4ffc23f6d6962440c
Andres B <andres.bustos@globant.com>
2021-08-23 01:13:21 -0500

    Css grid-Items

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -28,16 +28,18 @@ export const GifGrid = ({ category }) => {
     }
 
     return (
-        <div>
+        <>
             <h3>{ category }</h3>
-            {
-                images.map( img => (
-                    <GifGridItem
-                        {...img}
-                        key= { img.id }
-                    />
-                ))
-            }
-        </div>
+            <div className='card-grid'>
+                {
+                    images.map( img => (
+                        <GifGridItem
+                            {...img}
+                            key= { img.id }
+                        />
+                    ))
+                }
+            </div>
+        </>
     )
 }
