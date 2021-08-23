89389a84f922e208829b762f7f2f50926ca5a87a
Andres B <andres.bustos@globant.com>
2021-08-23 10:56:54 -0500

    Animaciones: - Uso Animate.css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -8,11 +8,11 @@ export const GifGrid = ({ category }) => {
 
     return (
         <>
-            <h3>{ category }</h3>
+            <h3 className= 'animate__animated animate__fadeIn'>{ category }</h3>
 
-            { loading && <p>Loading...</p>}
+            { loading && <p className= 'animate__animated animate__flash'>Loading...</p>}
 
-            <div className='card-grid'>
+            <div className= 'card-grid'>
                 {
                     data.map( img => (
                         <GifGridItem
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -11,12 +11,10 @@ export const useFetchGifs = ( category ) => {
     useEffect(() => {
         getGifs( category )
             .then( imgs => {
-                setTimeout( () => {
-                    setState({
-                        data: imgs,
-                        loading: false
-                    });
-                }, 2000);
+                setState({
+                    data: imgs,
+                    loading: false
+                });
             })
     }, [ category ])
 
